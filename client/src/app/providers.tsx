dfd57077063639e590d308b3ae73aebd2e00f622
Jas Shah <jasshah40@gmail.com>
2025-04-30 05:55:42 -0400

    Added AWS cognito authentication

--- client/src/app/providers.tsx
+++ client/src/app/providers.tsx
@@ -1,13 +1,17 @@
-'use client';
+"use client";
 
 import StoreProvider from "@/state/redux";
+import { Authenticator } from "@aws-amplify/ui-react";
+import Auth from "./(auth)/authProvider";
 
-const Providers = ({children} : {children: React.ReactNode}) => {
-    return (
-        <StoreProvider>
-            {children}
-        </StoreProvider>
-    )
-}
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <StoreProvider>
+      <Authenticator>
+        <Auth>{children}</Auth>
+      </Authenticator>
+    </StoreProvider>
+  );
+};
 
 export default Providers;
