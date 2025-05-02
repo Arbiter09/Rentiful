a62c87145ccad58931b6647a699c9b325a78eaaf
Jas Shah <jasshah40@gmail.com>
2025-05-01 21:57:51 -0400

    Completed Navbar with AWS cognito authentication

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,3 +1,6 @@
+"use client";
+
+import { createNewUserInDatabase } from "@/lib/utils";
 import { Manager, Tenant } from "@/types/prismaTypes";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { fetchAuthSession, getCurrentUser } from "aws-amplify/auth";
@@ -7,12 +10,12 @@ export const api = createApi({
     baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
     prepareHeaders: async (headers) => {
       const session = await fetchAuthSession();
-      const {idToken} = session.tokens ?? {};
+      const { idToken } = session.tokens ?? {};
       if (idToken) {
-        headers.set("Authorization" , `Bearer ${idToken}`)
+        headers.set("Authorization", `Bearer ${idToken}`);
       }
       return headers;
-    }
+    },
   }),
   reducerPath: "api",
   tagTypes: [],
@@ -33,20 +36,31 @@ export const api = createApi({
           let userDetailsResponse = await fetchWithBQ(endpoint);
 
           // if user doesn't exist, create new user
+          if (
+            userDetailsResponse.error &&
+            userDetailsResponse.error.status === 404
+          ) {
+            userDetailsResponse = await createNewUserInDatabase(
+              user,
+              idToken,
+              userRole,
+              fetchWithBQ
+            );
+          }
 
           return {
             data: {
-              cognitoInfo: {...user},
+              cognitoInfo: { ...user },
               userInfo: userDetailsResponse.data as Tenant | Manager,
-              userRole
-            }
-          }
-        } catch(error: any) {
-          return {error: error.message || "Could not fetch user data"}
+              userRole,
+            },
+          };
+        } catch (error: any) {
+          return { error: error.message || "Could not fetch user data" };
         }
-      }
-    })
+      },
+    }),
   }),
 });
 
-export const {} = api;
+export const { useGetAuthUserQuery } = api;
